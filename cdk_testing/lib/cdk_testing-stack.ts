46ee9205b1d6660b088bdcf6a061e065270275a3
danielm-buzz <102225291+danielm-buzz@users.noreply.github.com>
2022-03-29 16:07:44 +0300

    Add files via upload

--- cdk_testing/lib/cdk_testing-stack.ts
+++ cdk_testing/lib/cdk_testing-stack.ts
@@ -2,6 +2,7 @@ import {Construct, Stack, StackProps } from '@aws-cdk/core';
 // import { Construct } from 'constructs';
 // import { aws_s3 as s3 } from 'aws-cdk-lib';
 import { Bucket } from '@aws-cdk/aws-s3';
+import { RemovalPolicy } from 'aws-cdk-lib';
 
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -11,7 +12,8 @@ export class CdkTestingStack extends Stack {
     super(scope, id, props);
 
     const bucket = new Bucket(this, 'MyFirstBucket',{
-      bucketName:"myfirtsbuckete23"
+      bucketName:"myfirstbuckete23",
+      removalPolicy:RemovalPolicy.DESTROY
     });
 
   }
