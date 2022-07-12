8aca96bbabfb14fbc9f5de6848130c991ecc57d5
danielm-buzz <102225291+danielm-buzz@users.noreply.github.com>
2022-07-12 15:30:32 +0300

    Update cdk_testing.ts

--- cdk_testing/bin/cdk_testing.ts
+++ cdk_testing/bin/cdk_testing.ts
@@ -15,7 +15,7 @@ const app = new cdk.App();
 const params = app.node.tryGetContext("ctx1");
 console.log(params)
 const x= Buffer.from(params,'base64);
-let base64data = x.toString('ascii');
+const base64data = x.toString('ascii');
 
 console.log(base64data)
 const paramValue= JSON.parse(base64data);
